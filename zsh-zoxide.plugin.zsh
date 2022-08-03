5c0c59dc0c713b57e6a2121df1c79a39b0edb037
Salvydas Lukosius <sall@w-ss.io>
2022-08-03 17:44:00 +0100

    Update zsh-zoxide.plugin.zsh
    Signed-off-by: Salvydas Lukosius <sall@w-ss.io>

--- zsh-zoxide.plugin.zsh
+++ zsh-zoxide.plugin.zsh
@@ -1,3 +1,6 @@
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
+#
 # Zsh Plugin Standard
 # https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
@@ -16,25 +19,30 @@ if [[ ! -f ${Plugins[ZSH_ZOXIDE]}/man/man1/zoxide.1 ]]; then
 fi
 
 # When using Zi:
-# - The directory in which the database is stored.
-# - Manpages copied to set / default localtion.
-if (( ${ZI[SOURCED]} )) && [[ -d ${ZPFX} ]]; then
-  if [[ ! -d ${ZPFX}/share ]]; then
-    command mkdir -p ${ZPFX}/share || \
-    +zi-message "{error}Failed to create directory for database.{rst}"
+.zi-zoxide-prepare() {
+  builtin emulate -L zsh ${=${options[xtrace]:#off}:+-o xtrace}
+  builtin setopt extended_glob warn_create_global \
+  typeset_silent no_short_loops rc_quotes no_auto_pushd
+  # - The directory in which the database is stored.
+  # - Manpages copied to set / default localtion.
+  if (( ${ZI[SOURCED]} )) && [[ -d ${ZPFX} ]]; then
+    if [[ ! -d ${ZPFX}/share ]]; then
+      command mkdir -p ${ZPFX}/share || \
+      +zi-message "{error}Failed to create directory for database.{rst}"
+    fi
+    typeset -g _ZO_DATA_DIR=${ZPFX}/share
+    if [[ -d ${ZI[MAN_DIR]} ]] && [[ -d ${Plugins[ZSH_ZOXIDE]}/man/man1 ]]; then
+      cp ${Plugins[ZSH_ZOXIDE]}/man/man1/* ${ZI[MAN_DIR]}/man1/ || \
+      +zi-message "{error}Failed to install manpages.{rst}"
+    fi
   fi
-  _ZO_DATA_DIR=${ZPFX}/share
-  if [[ -d ${ZI[MAN_DIR]} ]] && [[ -d ${Plugins[ZSH_ZOXIDE]}/man/man1 ]]; then
-    cp ${Plugins[ZSH_ZOXIDE]}/man/man1/* ${ZI[MAN_DIR]}/man1/ || \
-    +zi-message "{error}Failed to install manpages.{rst}"
+
+  # Set zoxide commands x, xi when using with Zi.
+  if (( $ZI[SOURCED] )) && (( ${+functions[zi]} )); then
+    typeset -g _ZO_CMD_PREFIX=x
   fi
-fi
-
-# Set zoxide commands x, xi when using with Zi.
-if (( $ZI[SOURCED] )) && (( ${+functions[zi]} )); then
-  _ZO_CMD_PREFIX=x
-fi
-
+}
+.zi-zoxide-prepare
 # TODO: Env variables
 # If not set for Zi use default or user prefered.
 : ${_ZO_CMD_PREFIX:=$_ZO_CMD_PREFIX}
