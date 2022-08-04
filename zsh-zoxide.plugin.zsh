507d050b25b6ad91fbb65bf25c4a9f45ed36f1e5
Salvydas Lukosius <sall@w-ss.io>
2022-08-04 09:03:30 +0100

    [feat]: functions directory

--- zsh-zoxide.plugin.zsh
+++ zsh-zoxide.plugin.zsh
@@ -6,8 +6,17 @@
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
+# https://wiki.zshell.dev/community/zsh_plugin_standard#standard-plugins-hash
 typeset -gA Plugins
 Plugins[ZSH_ZOXIDE]="${0:h}"
+
+# https://wiki.zshell.dev/community/zsh_plugin_standard#funtions-directory
+if [[ $PMSPEC != *f* ]]; then
+  fpath+=( "${0:h}/functions" )
+fi
+
+# Autoload functions
+autoload -Uz "${0:h}/functions"/.*(.:t)
 
 if [[ ! -f ${Plugins[ZSH_ZOXIDE]}/man/man1/zoxide.1 ]]; then
   command git submodule --quiet init
@@ -19,30 +28,8 @@ if [[ ! -f ${Plugins[ZSH_ZOXIDE]}/man/man1/zoxide.1 ]]; then
 fi
 
 # When using Zi:
-.zi-zoxide-prepare() {
-  builtin emulate -L zsh ${=${options[xtrace]:#off}:+-o xtrace}
-  builtin setopt extended_glob warn_create_global \
-  typeset_silent no_short_loops rc_quotes no_auto_pushd
-  # - The directory in which the database is stored.
-  # - Manpages copied to set / default localtion.
-  if (( ${ZI[SOURCED]} )) && [[ -d ${ZPFX} ]]; then
-    if [[ ! -d ${ZPFX}/share ]]; then
-      command mkdir -p ${ZPFX}/share || \
-      +zi-message "{error}Failed to create directory for database.{rst}"
-    fi
-    typeset -g _ZO_DATA_DIR=${ZPFX}/share
-    if [[ -d ${ZI[MAN_DIR]} ]] && [[ -d ${Plugins[ZSH_ZOXIDE]}/man/man1 ]]; then
-      cp ${Plugins[ZSH_ZOXIDE]}/man/man1/* ${ZI[MAN_DIR]}/man1/ || \
-      +zi-message "{error}Failed to install manpages.{rst}"
-    fi
-  fi
+.zi-prepare-zoxide
 
-  # Set zoxide commands x, xi when using with Zi.
-  if (( $ZI[SOURCED] )) && (( ${+functions[zi]} )); then
-    typeset -g _ZO_CMD_PREFIX=x
-  fi
-}
-.zi-zoxide-prepare
 # TODO: Env variables
 # If not set for Zi use default or user prefered.
 : ${_ZO_CMD_PREFIX:=$_ZO_CMD_PREFIX}
@@ -58,8 +45,9 @@ fi
 # _ZO_MAXAGE
 # When set to 1, x will resolve symlinks before adding directories to the database.
 # _ZO_RESOLVE_SYMLINKS
+
 # Expand any tilde in the path.
-export _ZO_DATA_DIR=${~_ZO_DATA_DIR} 
+export _ZO_DATA_DIR=${~_ZO_DATA_DIR}
 
 # TODO: Output failures
 if (( $+commands[zoxide] )); then
